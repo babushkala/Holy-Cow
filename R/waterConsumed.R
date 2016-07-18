986bc42d449ea8c13a93610a9f6552aa029de2b7
CLAIRE ADAIR <cadair@princeton.edu>
2016-07-18 14:43:39 +0200

    Here is the first dataset and initialized script (looking at water consumed)

---
+++ R/waterConsumed.R
@@ -0,0 +1,4 @@
+# Read in waterConsumption data, showing values for water consumed in production of various ingredients
+library(readr)
+water_consumed <- read_csv("waterConsumption.csv")
+View(water_consumed)
